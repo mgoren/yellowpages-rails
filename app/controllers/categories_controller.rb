fed74fa2b37585b1bd9b86e2f23a0870b114252b
Luke Chinworth and Mike Goren <mgoren@mortalwombat.net>
2015-03-16 14:34:58 -0700

    add all restful paths for category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,52 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category successfully added!"
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category successfully updated!"
+      redirect_to category_path(@category)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    category = Category.find(params[:id])
+    category.destroy
+    flash[:notice] = "Category successfully eliminated."
+    redirect_to categories_path
+  end
+
+private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
